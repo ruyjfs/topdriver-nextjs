748406e871219f56d022e69e8e9fc7216e978558
Ruy <ruyjfs@gmail.com>
2025-11-08 07:52:13 -0300

    :hammer: Ajuste na tela de sobre.

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -9,18 +9,21 @@ export default function Page() {
       {/* <Header /> */}
       <section className="mx-auto max-w-5xl px-4 py-10">
         <h1 className="text-3xl font-bold">Olá, eu sou o Ruy 👋</h1>
-        <p className="mt-2 max-w-prose text-slate-300">
-          Sou completamente apaixonado por tecnologia e por onde conseguimos
-          chegar com ela.
-        </p>
-        <p>
-          Atuo como Arquiteto de Solução, Arquiteto de Software Fullstack
+        <p className="mt-2 text-slate-300">
+          Sou <b>engenheiro de software</b>, <b>arquiteto de soluções</b> e
+          criador do
+          <b> Religare Atomic App</b>.
+          {/* <p>
+          Já atuei como Arquiteto de Solução, Arquiteto de Software Fullstack
           Master, Analista de Sistemas Sr, Gerência de Configuração e Mudança
           (GCM) Desenvolvedor Sr e Engenheiro de Software.
+        </p> */}
+          Completamente apaixonado por tecnologia e por onde conseguimos chegar
+          com ela.
         </p>
-        <p className="mt-2 max-w-prose text-slate-300">
-          A melhor forma de prever o futuro, é criando ele e que possamos deixar
-          o mundo melhor do que o encontramos! 🍃🌎📜
+        <p className="mt-2 ">
+          Que possamos deixar o mundo melhor do que o encontramos e a melhor
+          forma de prever o futuro, é criando ele! 🍃🌎📜
         </p>
 
         <nav className="flex items-center gap-2 flex-1 justify-center p-10">
